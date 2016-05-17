7867c0f954fdf441912928eb71453d135f817d1c
cxh1440578160 <1440578160@qq.com>
2016-05-17 10:25:39 +0800

    将.gitignore添加到本地仓库

---
+++ CheikDemo/CheikDemo/OCDemo/OCDemoTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OCDemoTableViewController.h
+//  CheikDemo
+//
+//  Created by Cheik.chen on 16/5/17.
+//  Copyright © 2016年 cheik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OCDemoTableViewController : UITableViewController
+
+@end
--- CheikDemo/CheikDemo/OCDemo/OCDemoTableViewController.m
+++ CheikDemo/CheikDemo/OCDemo/OCDemoTableViewController.m
@@ -1,75 +1,50 @@
 //
-//  HomeTableViewController.m
+//  OCDemoTableViewController.m
 //  CheikDemo
 //
 //  Created by Cheik.chen on 16/5/17.
 //  Copyright © 2016年 cheik. All rights reserved.
 //
 
-#import "HomeTableViewController.h"
 #import "OCDemoTableViewController.h"
 
-@interface HomeTableViewController ()
+@interface OCDemoTableViewController ()
 {
     NSMutableArray *dataArray_;
 }
-
 @end
 
-@implementation HomeTableViewController
+@implementation OCDemoTableViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     dataArray_ = [NSMutableArray array];
     [self getData];
     [self.tableView reloadData];
-    
 }
-
 -(void)getData{
-
+    
     [dataArray_ addObject:@"OC语法"];
     [dataArray_ addObject:@"OCdemo"];
     [dataArray_ addObject:@"Switf语法"];
     [dataArray_ addObject:@"SwiftDemo"];
 }
-
 #pragma mark - Table view data source
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
     return dataArray_.count;
 }
 
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell" ];
     if(!cell){
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"UITableViewCell"];
     }
     cell.textLabel.text = dataArray_[indexPath.row];
+
+    
     return cell;
 }
 
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    [tableView deselectRowAtIndexPath:indexPath animated:YES];
-    switch (indexPath.row) {
-        case 0:
-            
-            break;
-        case 1:
-        {
-            OCDemoTableViewController *ocDemo = [[OCDemoTableViewController alloc]init];
-            [self.navigationController pushViewController:ocDemo animated:YES];
-        }
-            
-            break;
-        case 2:
-            
-            break;
-        case 3:
-            
-            break;
-        default:
-            break;
-    }
-}
 @end
